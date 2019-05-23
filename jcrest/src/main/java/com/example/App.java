eeaf7bb1efea03e5a62bd78cbd5fdc1e3a294f7d
Jc_Style <Jc_Style@121.141.47.66>
2019-05-23 22:40:57 +0900

    주석

--- jcrest/src/main/java/com/example/App.java
+++ jcrest/src/main/java/com/example/App.java
@@ -12,3 +12,22 @@ public class App {
         SpringApplication.run(App.class, args);
     }
 }
+
+/* 
+db table 생성
+ 
+create table members (
+no int(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+companyno int(20) NOT NULL
+name varchar(30)  NOT NULL,
+team varchar(30)  NOT NULL,
+position varchar(30)  NOT NULL,
+mrank varchar(30)  NOT NULL,
+
+
+ 메이븐 프로젝트 템플릿 생성 (CMD 창)
+mvn -B archetype:generate -DgroupId=com.example -DartifactId=jcrest -Dversion=1.0.0-SNAPSHOT -DarchetypeArtifactId=maven-archetype-quickstart
+
+
+
+);*/
